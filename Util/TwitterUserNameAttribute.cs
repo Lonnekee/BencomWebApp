18199a0d173790d6a791d270b1f2060edadd64ba
Lonneke Pulles <lonneke.pulles@gmail.com>
2019-07-10 22:05:46 +0200

    Final app 3

--- Util/TwitterUserNameAttribute.cs
+++ Util/TwitterUserNameAttribute.cs
@@ -13,7 +13,7 @@ namespace BencomWebApp.Util
         protected override ValidationResult IsValid(
         object value, ValidationContext validationContext)
         {
-            string userName = (string)value;
+            string userName = (string)value ?? "";
             Regex r = new Regex("^[a-zA-Z0-9_]+$"); // Checks for an alphanumeric expression with underscores and a length of at least 1.
             if (r.IsMatch(userName) && userName.Length <= Constants.MaxLengthTwitterUserName)
             {
