df4e61fe0aa55d834cbbd21395f222cebd174d9f
Lonneke Pulles <lonneke.pulles@gmail.com>
2019-07-10 17:02:22 +0200

    Final app

--- Util/Constants.cs
+++ Util/Constants.cs
@@ -7,10 +7,10 @@ namespace BencomWebApp.Util
 {
     public static class Constants
     {
-        public static const string BearerTokenTwitter = "AAAAAAAAAAAAAAAAAAAAAAnq%2FAAAAAAA55Pv3OaiypsfmLaDj%2BX0mzuZW8k%3D509Uq8EZojS9C49dqIW2u8kNWWUpmRMK3YUfvO6maonu6HfeBA";
-        public static const string AccessTokenInstagram = "360949981257304|uIKWn1PdZxXPZAx-Uqmqe3NBR_E";
-        public static const int MaxLengthTwitterUserName = 15;
-        public static const string TwitterImageBaseUrl = "https://t.co/";
-        public static const string TwitterURL = "https://api.twitter.com/";
+        public const string BearerTokenTwitter = "AAAAAAAAAAAAAAAAAAAAAAnq%2FAAAAAAA55Pv3OaiypsfmLaDj%2BX0mzuZW8k%3D509Uq8EZojS9C49dqIW2u8kNWWUpmRMK3YUfvO6maonu6HfeBA";
+        public const string AccessTokenInstagram = "360949981257304|uIKWn1PdZxXPZAx-Uqmqe3NBR_E";
+        public const int MaxLengthTwitterUserName = 15;
+        public const string TwitterImageBaseUrl = "https://t.co/";
+        public const string TwitterURL = "https://api.twitter.com/";
     }
 }
